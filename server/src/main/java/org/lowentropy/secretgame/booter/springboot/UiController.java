9bb320b9601e2ed340c679ba2553f39fd375445f
Laury Lafage <lowrey97x@hotmail.fr>
2020-12-06 23:07:32 +0100

    Fix get Room from url.

--- server/src/main/java/org/lowentropy/secretgame/booter/springboot/UiController.java
+++ server/src/main/java/org/lowentropy/secretgame/booter/springboot/UiController.java
@@ -2,12 +2,18 @@ package org.lowentropy.secretgame.booter.springboot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UiController {
 
-    @RequestMapping(value = { "/", "/create", "/join", "/room/*" })
+    @RequestMapping(value = { "/", "/create", "/join" })
     public String index() {
         return "index.html";
+    }
+
+    @RequestMapping(value = { "/room/**" })
+    public String room() {
+        return "/";
     }
 }
